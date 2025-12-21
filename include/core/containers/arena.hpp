084bba10d4612cbc270485d8517f9c6df3c00e2c
123Untitled <78928284+123Untitled@users.noreply.github.com>
2025-12-22 00:58:21 +0100

    implement arena container

--- include/core/containers/arena.hpp
+++ include/core/containers/arena.hpp
@@ -1,130 +1,95 @@
-#ifndef language_ast_storage_hpp
-#define language_ast_storage_hpp
+#ifndef arena_hpp
+#define arena_hpp
 
 #include "core/types.hpp"
 #include "core/memory/align_up.hpp"
-#include "language/tokens/iterator.hpp"
-#include "math.hpp"
 #include <vector>
 
 
-// -- A S  N A M E S P A C E --------------------------------------------------
+// -- M X  N A M E S P A C E --------------------------------------------------
 
-namespace as {
+namespace mx {
 
 
-	// -- S T O R A G E -------------------------------------------------------
+	// -- A R E N A -----------------------------------------------------------
 
-	class storage final {
+	class arena final {
 
 
 		private:
 
+			// -- private types -----------------------------------------------
+
+			/* self type */
+			using self = mx::arena;
+
+
 			// -- private members ---------------------------------------------
 
-			/* contiguous data */
 			std::vector<mx::u8> _data;
 
 
 		public:
 
-			// -- public modifiers --------------------------------------------
+			// -- public lifecycle --------------------------------------------
 
-			/* push frac
-			   push frac onto the storage */
-			auto push_frac(const mx::frac& frac) -> mx::usz {
+			/* default constructor */
+			arena(void) noexcept = default;
 
-				const mx::usz offset = mx::align_up<mx::frac>(_data.size());
-				constexpr auto  size = sizeof(mx::frac);
+
+			// -- public methods ----------------------------------------------
+
+			/* reset
+			   clear all stored data */
+			auto reset(void) noexcept -> void {
+				_data.clear();
+			}
+
+			/* reserve
+			   reserve space for at least size bytes */
+			auto reserve(const mx::usz size) -> void {
+				_data.reserve(size);
+			}
+
+
+			/* push
+			   push a new object of type T constructed with args
+			   returns the offset where the object was stored */
+			template <typename T, typename... Ts>
+			auto push(const Ts&... args) -> mx::usz {
+
+				// compute aligned offset
+				const mx::usz offset = mx::align_up<T>(_data.size());
 
 				// allocate space
-				_data.resize(offset + size);
+				_data.resize(offset + sizeof(T));
 
 				// construct in place
-				::new (_data.data() + offset) mx::frac{frac};
+				::new (&_data[offset]) T{args...};
 
 				return offset;
 			}
 
+			template <typename T>
+			auto at(const mx::usz index) /* noexcept */ -> T& {
 
-			/* push i8
-			   push i8 onto the storage */
-			auto push_i8(const mx::i8 byte) -> mx::usz {
+				if (index + sizeof(T) > _data.size())
+					throw std::runtime_error{"arena index out of bounds"};
 
-				const mx::usz offset = mx::align_up<mx::i8>(_data.size());
-				constexpr auto  size = sizeof(mx::i8);
-
-				// allocate space
-				_data.resize(offset + size);
-
-				// construct in place
-				::new (&_data[offset]) mx::i8{byte};
-
-				return offset;
+				return *reinterpret_cast<T*>(&_data[index]);
 			}
 
-			auto parse(tk::iterator& it,
-					   const tk::iterator& end) -> void {
+			template <typename T>
+			auto at(const mx::usz index) const /* noexcept */ -> const T& {
+
+				if (index + sizeof(T) > _data.size())
+					throw std::runtime_error{"arena index out of bounds"};
+
+				return *reinterpret_cast<const T*>(&_data[index]);
 			}
 
+	}; // class arena
 
-			// -- public accessors --------------------------------------------
+} // namespace mx
 
-			/* frac at
-			   access frac by direct index */
-			auto frac_at(const mx::usz index) const /*noexcept*/ -> const mx::frac& {
-				if (index + sizeof(mx::frac) > _data.size())
-					throw std::runtime_error{"Storage frac index out of bounds"};
-				return *reinterpret_cast<const mx::frac*>(&_data[index]);
-			}
-
-			/* i8 at
-			   access i8 by direct index */
-			auto i8_at(const mx::usz index) const /*noexcept*/ -> const mx::i8& {
-				if (index + sizeof(mx::i8) > _data.size())
-					throw std::runtime_error{"Storage i8 index out of bounds"};
-				return *reinterpret_cast<const mx::i8*>(&_data[index]);
-			}
-
-	}; // class storage
-
-	//class storage final {
-	//
-	//	private:
-	//		// -- private members ---------------------------------------------
-	//		std::vector<mx::u8> _data;
-	//
-	//	public:
-	//
-	//		storage(void) noexcept = default;
-	//
-	//
-	//		template <typename T>
-	//		auto push(const T& value) -> mx::usz {
-	//
-	//			const auto offset = mx::align_up<T>(_data.size());
-	//
-	//			// resize storage
-	//			_data.resize(offset + sizeof(T));
-	//
-	//			// pointer
-	//			void* ptr = _data.data() + offset;
-	//
-	//			// construct in place
-	//			::new (ptr) T{value};
-	//
-	//			return offset;
-	//		}
-	//
-	//		template <typename T>
-	//		auto at(const mx::usz index) const -> const T& {
-	//			if (index + sizeof(T) > _data.size()) {
-	//				throw std::runtime_error{"Storage index out of bounds"};
-	//			}
-	//			return *reinterpret_cast<const T*>(&_data[index]);
-	//		}
-	//};
-
-} // namespace as
-
-#endif // language_ast_storage_hpp
+#endif // arena_hpp
