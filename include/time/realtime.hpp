095e053f6edce2a96fd0b92690017df3edc3c4b2
123Untitled <78928284+123Untitled@users.noreply.github.com>
2025-12-22 00:52:17 +0100

    improve policy

--- include/time/realtime.hpp
+++ include/time/realtime.hpp
@@ -1,7 +1,7 @@
-#ifndef midilang_time_realtime_hpp
-#define midilang_time_realtime_hpp
+#ifndef realtime_hpp
+#define realtime_hpp
 
-#include "types.hpp"
+#include "core/types.hpp"
 
 
 // -- M L  N A M E S P A C E --------------------------------------------------
@@ -11,9 +11,68 @@ namespace mx {
 
 	// -- R E A L T I M E -----------------------------------------------------
 
-	/* realtime */
-	auto realtime(const mx::i64&) noexcept -> bool;
+	class realtime final {
+
+
+		private:
+
+			// -- private types -----------------------------------------------
+
+			/* self type */
+			using self = mx::realtime;
+
+
+			// -- private members ---------------------------------------------
+
+			/* min computation */
+			mx::u32 _min_rt_quantum;
+
+			/* max computation */
+			mx::u32 _max_rt_quantum;
+
+
+			/* period */
+			mx::u32 _period;
+
+			/* computation */
+			mx::u32 _computation;
+
+			/* constraint */
+			mx::u32 _constraint;
+
+
+			// -- private static methods --------------------------------------
+
+			/* minimum rt computation */
+			static auto min_rt_quantum(void) -> mx::u32;
+
+			/* maximum rt computation */
+			static auto max_rt_quantum(void) -> mx::u32;
+
+
+		public:
+
+			// -- public lifecycle --------------------------------------------
+
+			/* default constructor */
+			realtime(void) noexcept;
+
+
+			// -- public methods ----------------------------------------------
+
+			/* apply */
+			auto apply(void) noexcept -> bool;
+
+
+			// -- public modifiers --------------------------------------------
+
+			/* period */
+			auto period(const mx::u32 value) noexcept -> void {
+				_period = value;
+			}
+
+	}; // class realtime
 
 } // namespace mx
 
-#endif // midilang_time_realtime_hpp
+#endif // realtime_hpp
--- sources/time/realtime.cpp
+++ sources/time/realtime.cpp
@@ -4,43 +4,56 @@
 #include <mach/mach.h>
 
 
-/* realtime */
-auto mx::realtime(const mx::i64& nano_clock) noexcept -> bool {
+// -- R E A L T I M E ---------------------------------------------------------
+
+
+// -- private static methods --------------------------------------------------
+
+/* minimum rt computation */
+auto mx::realtime::min_rt_quantum(void) -> mx::u32 {
+	// 50 us
+	return static_cast<mx::u32>(mx::timebase::us_to_ticks(50U));
+}
+
+/* maximum rt computation */
+auto mx::realtime::max_rt_quantum(void) -> mx::u32 {
+	// 50 ms
+	return static_cast<mx::u32>(mx::timebase::ms_to_ticks(50U));
+}
+
+
+// -- public methods ----------------------------------------------------------
+
+/* apply */
+auto mx::realtime::apply(void) noexcept -> bool {
 
 	// policy data
 	::thread_time_constraint_policy_data_t policy {
 
-		/* This is the nominal amount of time between separate
-		// processing arrivals, specified in absolute time units.
-		// A value of 0 indicates that there is no inherent periodicity
-		// in the computation.
-		*/  //policy.period		= 50 * msToAbsolute; // 50 ms periodicity - time between
-		.period = static_cast<mx::u32>(nano_clock * mx::timebase::nano_to_absolute()),
-		// chatGPR recommendation
-		/* Le champ period de la structure thread_time_constraint_policy_data_t spécifie la durée nominale entre les arrivées de traitement séparées, en unités de temps absolues. Cela signifie que cette valeur indique la fréquence à laquelle le thread en cours est censé être exécuté.
-		Par exemple, si la valeur de period est définie sur 50 * msToAbsolute, cela signifie que le thread en cours est censé être exécuté toutes les 50 millisecondes.
-		Une valeur de 0 pour period indique qu'il n'y a pas de périodicité inhérente dans le calcul du thread en cours. Cela signifie que le thread n'a pas de fréquence d'exécution fixe et peut être exécuté à n'importe quel moment.
-		*/
+		/* period: This is the nominal amount of time between separate
+		 * processing arrivals, specified in absolute time units.  A
+		 * value of 0 indicates that there is no inherent periodicity in
+		 * the computation. */
+		.period = _period,
 
+		/* computation: This is the nominal amount of computation
+		 * time needed during a separate processing arrival, specified
+		 * in absolute time units.  The thread may be preempted after
+		 * the computation time has elapsed.
+		 * If (computation < constraint/2) it will be forced to
+		 * constraint/2 to avoid unintended preemption and associated
+		 * timer interrupts. */
+		.computation = _computation,
 
-		/* This is the nominal amount of computation
-		// time needed during a separate processing arrival, specified
-		// in absolute time units.
-		*/ //policy.computation	= 1 * msToAbsolute; // 1 ms of work
-		.computation = static_cast<mx::u32>(1 * mx::timebase::ms_to_absolute()), // 1 ms of work
-		//Il est également important de tenir compte de la précision de l'horloge requise par votre application. Si une précision élevée est requise, une valeur de computation plus grande peut être nécessaire pour garantir que le thread a suffisamment de temps pour effectuer tous les calculs nécessaires. Cependant, si une précision moins élevée est acceptable, une valeur de computation plus petite peut être utilisée pour réduire la charge de travail du processeur.
+		/* constraint: This is the maximum amount of real time that
+		 * may elapse from the start of a separate processing arrival
+		 * to the end of computation for logically correct functioning,
+		 * specified in absolute time units.  Must be (>= computation).
+		 * Note that latency = (constraint - computation). */
+		.constraint = _constraint,
 
-		/* This is the maximum amount of real time that
-		// may elapse from the start of a separate processing arrival
-		// to the end of computation for logically correct functioning,
-		// specified in absolute time units.  Must be (>= computation).
-		// Note that latency = (constraint - computation).
-		*/ //policy.constraint	= 2 * msToAbsolute;
-		.constraint = static_cast<mx::u32>(1 * mx::timebase::ms_to_absolute()),
-
-		/* This indicates that the computation may be
-		// interrupted, subject to the constraint specified above.
-		*/
+		/* preemptible: IGNORED (This indicates that the computation may be
+		 * interrupted, subject to the constraint specified above.) */
 		.preemptible = true
 	};
 
@@ -49,12 +62,26 @@ auto mx::realtime(const mx::i64& nano_clock) noexcept -> bool {
 	const auto self_thread = ::mach_thread_self();
 
 	// set thread policy
-	return ::thread_policy_set(self_thread,
+	const auto ret = ::thread_policy_set(self_thread,
 							   // real-time behavior
 							   THREAD_TIME_CONSTRAINT_POLICY,
 							   // policy data
 							   reinterpret_cast<::thread_policy_t>(&policy),
 							   // policy count
-							   THREAD_TIME_CONSTRAINT_POLICY_COUNT)
-		!= KERN_SUCCESS;
+							   THREAD_TIME_CONSTRAINT_POLICY_COUNT);
+
+
+	return ret == KERN_SUCCESS;
+}
+
+
+// -- public lifecycle --------------------------------------------------------
+
+/* default constructor */
+mx::realtime::realtime(void) noexcept
+: _min_rt_quantum{min_rt_quantum()},
+  _max_rt_quantum{max_rt_quantum()},
+  _period{0U},
+  _computation{_min_rt_quantum},
+  _constraint{_min_rt_quantum * 2U} {
 }
