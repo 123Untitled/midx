f99fdd7f1f53ae3d80f6c5673949bdb95c5cf4e2
123Untitled <78928284+123Untitled@users.noreply.github.com>
2025-12-22 02:53:29 +0100

    feat(coremidi): add descriptive error messages to exceptions
    Map OSStatus codes to human-readable messages. Exception constructor
    now concatenates location with error description using inplace_string.

--- include/coremidi/exception.hpp
+++ include/coremidi/exception.hpp
@@ -2,12 +2,12 @@
 #define coremidi_exception_hpp
 
 #include "os.hpp"
+#include "core/string/inplace_string.hpp"
 
 #if defined(midx_macos)
 
 #include <exception>
 #include "coremidi/types.hpp"
-#include <CoreMIDI/CoreMIDI.h>
 
 
 // -- C O R E M I D I  N A M E S P A C E --------------------------------------
@@ -34,7 +34,7 @@ namespace cm {
 			cm::os_status _code;
 
 			/* error message */
-			const char* _what;
+			mx::inplace_string<1024U> _msg;
 
 
 		public:
@@ -45,8 +45,9 @@ namespace cm {
 			exception(void) noexcept = delete;
 
 			/* code constructor */
-			exception(const ::OSStatus code, const char* what) noexcept
-			: std::exception{}, _code{code}, _what{what} {
+			template <unsigned N>
+			exception(const ::OSStatus code, const char (&where)[N]) noexcept
+			: std::exception{}, _code{code}, _msg{where, ": ", _midi_error(code)} {
 			}
 
 			/* copy constructor */
@@ -77,7 +78,53 @@ namespace cm {
 
 			/* what */
 			auto what(void) const noexcept -> const char* override {
-				return _what;
+				return _msg.data();
+			}
+
+
+		private:
+
+			// -- private static methods --------------------------------------
+
+			/* midi error */
+			constexpr auto _midi_error(const cm::os_status err) -> const char* {
+
+				switch (err) {
+					case kMIDIInvalidClient:
+						return "The client is invalid.";
+					case kMIDIInvalidPort:
+						return "The port is invalid.";
+					case kMIDIWrongEndpointType:
+						return "A function received a source endpoint when it required a destination endpoint, or vice versa.";
+					case kMIDINoConnection:
+						return "The connection you’re trying to close doesn’t exist.";
+					case kMIDIUnknownEndpoint:
+						return "The system doesn’t recognize the endpoint.";
+					case kMIDIUnknownProperty:
+						return "The property you’re trying to query isn’t set on the object.";
+					case kMIDIWrongPropertyType:
+						return "The value you assigned to the property is the wrong type.";
+					case kMIDINoCurrentSetup:
+						return "A MIDI setup object doesn’t currently exist.";
+					case kMIDIMessageSendErr:
+						return "The communication with the MIDI server failed.";
+					case kMIDIServerStartErr:
+						return "The system can’t start the MIDI server.";
+					case kMIDISetupFormatErr:
+						return "The system can’t read the saved state.";
+					case kMIDIWrongThread:
+						return "A driver is calling a non-I/O function in the server from a thread other than the server’s main thread.";
+					case kMIDIObjectNotFound:
+						return "The requested object doesn’t exist.";
+					case kMIDIIDNotUnique:
+						return "The identifier you’re trying to set isn’t unique.";
+					case kMIDINotPermitted:
+						return "The process doesn’t have privileges for the requested operation.";
+					case kMIDIUnknownError:
+						return "The system can’t perform the requested operation.";
+					default:
+						return "Unknown error.";
+				}
 			}
 
 	}; // class exception
